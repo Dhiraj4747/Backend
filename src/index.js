167eda56b47b61cf06ed05cf3318431f6b476805
Dhiraj Patil <dhirajpatil1353@gmail.com>
2024-09-19 06:32:21 +0530

    JWT-changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import dotenv from "dotenv";
 import connectDB from "./db/dbconnection.js";
-import { DB_NAME } from "./constants.js";
+import app from "./app.js";
 
 dotenv.config({
    path: "./env"
 })
 
-connectDB();
+connectDB()
+.then(()=> {
+   app.listen(process.env.PORT || 3000, ()=>{
+      console.log(`Server is running ${process.env.PORT}`);
+   })
+})
+.catch((err)=>{
+   console.log("Failed To Precess BRO ",err)
+})
+
+
+
+
+
+
+
 
 // (async ()=>{
 //    try{
